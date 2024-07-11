69db9ef72acd4675e4b2af2372e4a46647be71c8
Garima Chaubey <134435393+GarimaChaubey@users.noreply.github.com>
2024-07-11 10:29:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Upper case conversion/upper-case-conversion.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+string transform(string str);
+int main()
+{
+    int t;
+    cin>>t;
+    cin.ignore();
+    while(t--)
+    {
+        string str;
+        getline(cin,str);
+        cout<<transform(str)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+
+string transform(string s)
+{
+    // code here
+    
+    bool flag=true;
+    
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]==' ')
+        {
+        flag=true;
+        }
+        else if(flag)
+        {
+            s[i]=toupper(s[i]);
+            flag=false;   
+        }
+    }
+    return s;
+}
