5b28fb3bb2d2e1545ad8e8404b300dadd5b87bcf
Garima Chaubey <134435393+GarimaChaubey@users.noreply.github.com>
2024-07-16 09:26:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Java Strings | Set 1/java-strings-set-1.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string conRevstr(string S1, string S2) {
+        // code here
+        
+        string res= S1+S2;
+        string rev="";
+        
+        for(int i=res.length()-1;i>=0;i--)
+        {
+            rev+=res[i];
+        }
+        
+        return rev;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S1, S2;
+        cin >> S1;
+        cin >> S2;
+        Solution ob;
+        cout << ob.conRevstr(S1, S2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
