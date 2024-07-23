92153da3e944cba983c2aacb1471ec3b2d4ffa9e
Garima Chaubey <134435393+GarimaChaubey@users.noreply.github.com>
2024-07-23 09:37:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Binary representation/binary-representation.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    string getBinaryRep(int N)
+    {
+        // Write Your Code here
+        
+        
+        string str="";
+        
+        for(int i=0;i<30;i++)
+        {
+            str=to_string(N%2)+str;
+            N/=2;
+        }
+        return str;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+       
+        Solution ob;
+        string ans = ob.getBinaryRep(N);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
