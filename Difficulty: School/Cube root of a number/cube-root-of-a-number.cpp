3c2e130a083d8ac8d4ac00a619bb924a4c4a3345
Garima Chaubey <134435393+GarimaChaubey@users.noreply.github.com>
2024-07-25 11:24:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Cube root of a number/cube-root-of-a-number.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int cubeRoot(int N) {
+        // code here
+        
+        return pow(N,1.0/3.0);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+
+        cin>>N;
+
+        Solution ob;
+        cout << ob.cubeRoot(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
