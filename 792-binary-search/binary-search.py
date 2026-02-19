71f1676c8eeb9c28af54663fdf376e5cc17f7b1f
Garima Chaubey <134435393+GarimaChaubey@users.noreply.github.com>
2026-02-19 15:47:03 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (5.80%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def search(self, nums, target):
+        low=0
+        high=len(nums)-1
+        
+
+        while(low<=high):
+            mid=(low+high)//2
+            if(nums[mid]==target):
+                return mid
+            elif(target>nums[mid]):
+                low=mid+1
+            elif(target<nums[mid]):
+                high=mid-1
+
+        return -1
