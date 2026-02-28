9954b4ce01277d1e8d7d4f1d1cac5618800a330b
Garima Chaubey <134435393+GarimaChaubey@users.noreply.github.com>
2026-02-28 19:27:25 +0530

    Time: 535 ms (83.33%) | Memory: 15.5 MB (91.67%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def concatenatedBinary(self, n):
+
+        MOD = 10**9 + 7
+        ans = 0
+        length = 0
+
+        for i in range(1, n + 1):
+
+            # if i is power of 2
+            if (i & (i - 1)) == 0:
+                length += 1
+
+            ans = ((ans << length) | i) % MOD
+
+        return ans
