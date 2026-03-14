12a8844d90f4279d83f7466d18274ddb271ca95d
Garima Chaubey <134435393+GarimaChaubey@users.noreply.github.com>
2026-03-14 14:19:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (98.40%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        def mirror(l,r):
+            if not l and not r:
+                return True
+            if not l or not r:
+                return False
+            if(l.val==r.val) and mirror(l.left, r.right) and mirror(l.right, r.left):
+                return True
+            return False
+        return mirror(root.left, root.right)
