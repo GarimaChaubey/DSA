4ce0e02979fc4b11ce9f78a88835fb58b2849e74
Garima Chaubey <134435393+GarimaChaubey@users.noreply.github.com>
2026-03-14 14:06:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (99.15%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+class Solution(object):
+    def rightSideView(self, root):
+
+        if not root:
+            return []
+
+        q = deque([root])
+        res = []
+
+        while q:
+
+            size = len(q)
+
+            for i in range(size):
+
+                node = q.popleft()
+
+                if i == size - 1:
+                    res.append(node.val)
+
+                if node.left:
+                    q.append(node.left)
+
+                if node.right:
+                    q.append(node.right)
+
+        return res
